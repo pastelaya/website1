5f435b80e8c4c7150fa287c9b7dbad1c36dc6f4b
pastelaya <142944386+pastelaya@users.noreply.github.com>
2024-02-27 14:55:07 -0800

    loading

---
+++ js/loading.js
@@ -0,0 +1,20 @@
+loadText = document.querySelector('.text')
+bg = document.querySelector('.bg')
+
+load = 0
+
+int = setInterval(blurring, 35)
+
+
+function blurring() {
+    load++
+
+    if (load > 99){
+        clearInterval(int)
+    }
+    loadText.innerText = `${load}%`
+
+
+    loadText.style.opacity = 1 - load/100
+    bg.style.filter = `blur(${30 - load/100*30}px)`
+}
