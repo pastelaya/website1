8c62e928d91cbfceb03aea204b070c8bad3bbd3e
pastelaya <142944386+pastelaya@users.noreply.github.com>
2024-02-29 22:33:41 +0000

    Thu, Feb 29, 2024, 2:33 PM -08:00

--- js/expanding.js
+++ js/expanding.js
@@ -2,7 +2,16 @@ panels = document.querySelectorAll('.panel')
 
 panels.ForEach(panel => {
     panel.addEventListener('click', () => {
+        removeActive()
         panel.classList.add('active')
     })
 
 })
+
+
+
+function removeActive(){
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
