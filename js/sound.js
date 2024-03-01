971fcffbdfcda911840618df91ad007773833286
pastelaya <142944386+pastelaya@users.noreply.github.com>
2024-03-01 22:49:49 +0000

    Fri, Mar 1, 2024, 2:49 PM -08:00

--- js/sound.js
+++ js/sound.js
@@ -3,9 +3,9 @@ sound = ['boo', 'boo', 'boo', 'boo', 'boo', 'boo']
 sound.forEach(sound => {
     btn = document.createElement('button')
     btn.classList.add('btn')
-    btn.innetText=sound
+    btn.innerText=sound
     btn.addEventListener('click', ()=> {
         document.getElementById(sound).play()
     })
-    document.getElementById('Buttons').appendChild(btn)
+    document.getElementById('buttons').appendChild(btn)
 })
