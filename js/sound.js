5e5e00e678844534dd5fc401f57a68b8e844ccbd
pastelaya <142944386+pastelaya@users.noreply.github.com>
2024-03-14 22:02:31 +0000

    Thu, Mar 14, 2024, 3:02 PM -07:00

--- js/sound.js
+++ js/sound.js
@@ -1,4 +1,4 @@
-sounds = ['bo01', 'bo02', 'bo03', 'bo04', 'bo05', 'bo06', 'bo07', 'bo08', 'bo09', 'bo10', 'bo11']
+sounds = ['bo01', 'bo02', 'bo03', 'bo04', 'bo05', 'bo06', 'bo07', 'bo08', 'bo09', 'bo10', 'bo11', 'bo12', 'bo13']
 
 sounds.forEach(sound => {
     btn = document.createElement('button')
