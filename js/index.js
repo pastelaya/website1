10c0ec94c095c62ad3747f2647342134b2396ad7
pastelaya <142944386+pastelaya@users.noreply.github.com>
2024-03-15 21:56:15 +0000

    Fri, Mar 15, 2024, 2:56 PM -07:00

--- js/index.js
+++ js/index.js
@@ -1,2 +1,14 @@
 var list = document.getElementsByClassName('list'),
 hovered = false;
+
+function AnimationListener() {
+    if(hovered)
+    {
+      list.classList.remove('animated');
+      list.style.webkitTransform = 'scale(2)';
+      list.style.MozTransform = 'scale(2)';
+      list.style.msTransform = 'scale(2)';
+      list.style.OTransform = 'scale(2)';
+      list.style.transform = 'scale(2)';
+    }
+}
